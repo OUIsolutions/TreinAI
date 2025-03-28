2789b4cd3187fb26d31c9e76dbce99aa07f863f1
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-28 19:19:36 -0300

    att

---
+++ src/ai_functions/move_item/fdefine.move_item.c
@@ -0,0 +1,24 @@
+//silver_chain_scope_start
+//DONT MODIFY THIS COMMENT
+//this import is computationally generated
+//mannaged by SilverChain: https://github.com/OUIsolutions/SilverChain
+#include "../../imports/imports.globals.h"
+//silver_chain_scope_end
+
+char *agent_move_item(cJSON *args, void *pointer){
+    cJSON *src = cJSON_GetObjectItem(args, "src");
+    cJSON *dest = cJSON_GetObjectItem(args, "dest");
+    if(!cJSON_IsString(src) || !cJSON_IsString(dest)){
+        return NULL;
+    }
+    dtw.move_any(src->valuestring, dest->valuestring,false);
+    printf("%s AI MOVED: %s to %s\n",YELLOW, src->valuestring, dest->valuestring, RESET);
+    return (char*)"item moved";
+}
+
+void configure_move_item_callbacks(OpenAiInterface *openAi,const char *model){
+    OpenAiCallback *callback = new_OpenAiCallback(agent_move_item, (void*)model, "move", "move a file or directory", false);
+    OpenAiInterface_add_parameters_in_callback(callback, "src", "Pass the source path.", "string", true);
+    OpenAiInterface_add_parameters_in_callback(callback, "dest", "Pass the destination path.", "string", true);
+    OpenAiInterface_add_callback_function_by_tools(openAi, callback);
+}
