4f495c3c93f93e552e38b5628d677ca539628ca5
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-28 02:29:06 -0300

    att

--- src/ai_functions/execute_command/fdefine.execute_command.c
+++ src/ai_functions/execute_command/fdefine.execute_command.c
@@ -5,39 +5,43 @@
 #include "../../imports/imports.globals.h"
 //silver_chain_scope_end
 char *agent_execute_command(cJSON *args, void *pointer){
+    
     const char *model = (const char*)pointer;
     cJSON *command = cJSON_GetObjectItem(args, "command");
     if(!cJSON_IsString(command)){
         return NULL;
     }
-    int result = system(command->valuestring);
     printf("%s %s EXECUTED COMMAND: %s\n", YELLOW, model, command->valuestring, RESET);
     const int READ_SIZE = 100;
-
     char *output = calloc(READ_SIZE+1,sizeof(char));
+
     int output_allocated = READ_SIZE+1;
     int output_size = 0;
     FILE *fp = popen(command->valuestring, "r");
-    if (fp) {
-        while(true){
-            if(output_size + READ_SIZE > output_allocated){
-                output_allocated = READ_SIZE + (output_allocated * 2);
-                output = realloc(output, output_allocated);
-            }
-
-            int total_read =  fread(output + output_size, 1, READ_SIZE-1, fp);
-            if(total_read <= 0){
-                break;
-            }
-            output_size += total_read;
-            output[output_size] = '\0';
-        }
-        fclose(fp);
+    if (fp == NULL) {
+        free(output);
+        return NULL; 
     }
-    printf("chegou aqui\n");
+    while(true){
+        if((output_size + READ_SIZE) > output_allocated){
+            output_allocated = READ_SIZE + (output_allocated * 2);
+            output = realloc(output, output_allocated);
+        }
+
+        int total_read =  fread(output + output_size, sizeof(char), READ_SIZE-1, fp);
+        printf("total_read: %d\n", total_read);
+        if(total_read <= 0){
+            break;
+        }
+
+        output_size += total_read;
+        output[output_size] = '\0';
+
+    }
+
+    int result = pclose(fp);
     char *result_str = (char*)malloc(strlen(output) + 100);
-    sprintf(result_str, "status: %s\nOutput: %s", result, output);
-    printf("%s %s OUTPUT: %s\n", YELLOW, model, output);
+    sprintf(result_str, "status: %d\nOutput:\n%s", result, output);
     free(output);
     return result_str;
 }
