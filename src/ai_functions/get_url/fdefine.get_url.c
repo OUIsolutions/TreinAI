99ab13a313ee7ca6b0435e204d021d17e82fda5f
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-29 19:51:18 -0300

    att

---
+++ src/ai_functions/get_url/fdefine.get_url.c
@@ -0,0 +1,40 @@
+//silver_chain_scope_start
+//DONT MODIFY THIS COMMENT
+//this import is computationally generated
+//mannaged by SilverChain: https://github.com/OUIsolutions/SilverChain
+#include "../../imports/imports.globals.h"
+//silver_chain_scope_end
+
+
+char *agent_get_url(cJSON *args, void *pointer){
+    const char *model = (const char*)pointer;
+    cJSON *url = cJSON_GetObjectItem(args, "url");
+    if(!cJSON_IsString(url)){
+        return NULL;
+    }
+    BearHttpsRequest *request = bear.request.newBearHttpsRequest(url->valuestring);
+    BearHttpsResponse *response = bear.request.fetch(request);
+    if(bear.response.error(response)){
+        char *error = strdup(bear.response.get_error_msg(response));
+        bear.response.free(response);
+        bear.request.free(request);
+        return error;
+    }
+    const char *body = bear.response.read_body_str(response);
+    if(bear.response.error(response)){
+        char *error = strdup(bear.response.get_error_msg(response));
+        bear.request.free(request);
+        bear.response.free(response);
+        return error;
+    }
+    char *bdy_copy = strdup(body);
+    bear.request.free(request);
+    bear.response.free(response);
+    return bdy_copy;
+}
+
+void configure_get_url(OpenAiInterface *openAi,const char *model){
+    OpenAiCallback *callback = new_OpenAiCallback(agent_get_url,(void*)model, "get_url", "fetch a url", false);
+    OpenAiInterface_add_parameters_in_callback(callback, "url", "Pass the url you want to fetch.", "string", true);
+    OpenAiInterface_add_callback_function_by_tools(openAi, callback);
+}
