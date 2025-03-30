edd07a94a57ef7eb336327ff8865cf38f10d906a
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-30 17:17:16 -0300

    att

--- src/ai_functions/deep_search/fdefine.deep_search.c
+++ src/ai_functions/deep_search/fdefine.deep_search.c
@@ -6,9 +6,19 @@
 //silver_chain_scope_end
 
 
-
+char *agent_set_rate(cJSON *args, void *pointer){
+    cJSON *rate = cJSON_GetObjectItem(args, "rate");
+    if(!cJSON_IsNumber(rate)){
+        return NULL;
+    }
+    int *rate_pointer = (int*)pointer;
+    *rate_pointer = rate->valuedouble;
+    return "rate setted";
+}
 
 char *agent_deep_search(cJSON *args, void *pointer){
+    ModelProps *props = (ModelProps*)pointer;
+
     cJSON *question = cJSON_GetObjectItem(args, "question");
     if(!cJSON_IsString(question)){
         return NULL;
@@ -23,13 +33,27 @@ char *agent_deep_search(cJSON *args, void *pointer){
     for(int i = 0; i < doc_dir->size; i++){
         char *path = doc_dir->strings[i];
         char *content = dtw.load_string_file_content(path);
+        int rate;
+        OpenAiInterface *openAi =openai.openai_interface.newOpenAiInterface(props->url, props->key,props->model);
+        
+        OpenAiCallback *callback = new_OpenAiCallback(agent_set_rate, &rate, "set_rate", "set the rate of the model", false);
+        OpenAiInterface_add_parameters_in_callback(callback, "rate", "The rate you want to set", "number", true);
+        OpenAiInterface_add_callback_function_by_tools(openAi, callback);
 
-        OpenAiInterface *openAi =openai.openai_interface.newOpenAiInterface()
+        openai.openai_interface.add_system_prompt(openAi,"your function its to clasify how useful is the document for the question");
+        
+        char *question_str = malloc(strlen(question->valuestring) + 100);
+        sprintf(question,"question: %s",question->valuestring);
+        openai.openai_interface.add_system_prompt(openAi,question);
 
-        free(content);
+        char *content = malloc(strlen(content) + 100);
+        sprintf(content,"content: %s",content);
+        openai.openai_interface.add_system_prompt(openAi,content);
+
+        
     }
     
-    return knolage_digestor->actual_response;
+    return "not found";
 }
 
 void configure_deep_search(OpenAiInterface *openAi,ModelProps *model){
