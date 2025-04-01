11827708cc471414b338608aafda2f195eee47d9
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-01 05:41:40 -0300

    att

---
+++ src/config/const.colors.c
@@ -0,0 +1,22 @@
+//silver_chain_scope_start
+//DONT MODIFY THIS COMMENT
+//this import is computationally generated
+//mannaged by SilverChain: https://github.com/OUIsolutions/SilverChain
+#include "../imports/imports.dep_declare.h"
+//silver_chain_scope_end
+
+#ifdef __linux__
+const char* GREEN = "\033[0;32m";
+const char* BLUE = "\033[0;34m";
+const char* YELLOW = "\033[0;33m";
+const char* RED = "\033[0;31m";
+const char* RESET = "\033[0m";
+const char* PURPLE = "\033[0;35m";
+#else
+const char* GREEN = "";
+const char* BLUE = "";
+const char* YELLOW = "";
+const char* RED = "";
+const char* RESET = "";
+const char* PURPLE = "";
+#endif
