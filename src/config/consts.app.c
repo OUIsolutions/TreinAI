8a1cda6f366b17321d1b7c37cf9c70bcd3d25f84
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-01 05:44:38 -0300

    attt

--- src/config/consts.app.c
+++ src/config/consts.app.c
@@ -2,7 +2,7 @@
 //DONT MODIFY THIS COMMENT
 //this import is computationally generated
 //mannaged by SilverChain: https://github.com/OUIsolutions/SilverChain
-#include "../imports/imports.dep_declare.h"
+#include "../imports/imports.types.h"
 //silver_chain_scope_end
 
 const char *VERSION ="0.0.3";
