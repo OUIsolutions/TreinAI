0357eea8e5cd00760c8029a7fe75420025fcaafd
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-01 06:30:30 -0300

    att

--- src/config/consts.colors.c
+++ src/config/consts.colors.c
@@ -7,7 +7,7 @@
 
 #ifdef __linux__
 const char* GREEN = "\033[0;32m";
-const char* BLUE = "\033[0;34m";
+const char* BLUE = "\033[0;96m";
 const char* YELLOW = "\033[0;33m";
 const char* RED = "\033[0;31m";
 const char* RESET = "\033[0m";
