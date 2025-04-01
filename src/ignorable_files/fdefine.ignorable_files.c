e31471fb909658003765a6ddcf282eed69963fe9
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-01 08:00:01 -0300

    att

--- src/ignorable_files/fdefine.ignorable_files.c
+++ src/ignorable_files/fdefine.ignorable_files.c
@@ -8,4 +8,41 @@
 
 DtwStringArray *list_files_recursively_not_incluidng_ignorable_files(const char *listage_path){
     
+    Asset *ignorable_files = get_asset("ignorable_files.json");
+    if(!ignorable_files){
+        printf("%sError: %s%s\n", RED, "File ignorable_files.json not found", RESET);
+        return NULL;
+    }
+    cJSON *parsed_ignorable_files = cJSON_Parse(ignorable_files->data);
+    if(!parsed_ignorable_files){
+        printf("%sError: %s%s\n", RED, "Error parsing ignorable_files.json", RESET);
+        return NULL;
+    }
+    int total_ignorable_files = cJSON_GetArraySize(parsed_ignorable_files);
+
+    DtwStringArray *filtered = newDtwStringArray();
+    DtwStringArray *all= list_files_recursively(listage_path,true);
+    for (int i = 0; i < all->size; i++){
+        bool ignore = false;
+        char *file = all->strings[i];
+        CTextStack *converted_file = newCTextStack_string(file);
+        for(int j = 0; j < total_ignorable_files; j++){
+            cJSON *ignorable_file = cJSON_GetArrayItem(parsed_ignorable_files, j);
+            if(CTextStack_index_of(converted_file,ignorable_file->valuestring) != -1){
+                ignore = true;
+                break;
+            }
+        }
+
+        if(ignore){
+            CTextStack_free(converted_file);
+            continue;
+        }
+        
+        DtwStringArray_append(filtered, file);
+        CTextStack_free(converted_file);
+    }
+    
+    DtwStringArray_free(all);
+    return filtered;
 }
