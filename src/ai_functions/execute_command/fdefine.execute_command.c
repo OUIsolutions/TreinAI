f51d412caf5f41f1c4b26178e4359f057d824c8e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-01 14:59:30 -0300

    adappting_comand

--- src/ai_functions/execute_command/fdefine.execute_command.c
+++ src/ai_functions/execute_command/fdefine.execute_command.c
@@ -19,12 +19,16 @@ char *agent_execute_command(cJSON *args, void *pointer){
     
     char *full_command = (char*)malloc(strlen(command->valuestring) + 100);
     if(!full_command){
-        return "Error internal.";
+        return strdup("Error internal.");
     }
     sprintf(full_command, "%s  &> cache.txt", command->valuestring);
     int result = system(full_command);    
-    char *output = dtw.load_string_file_content("cache.txt");
     free(full_command);
+    char *output = dtw.load_string_file_content("cache.txt");
+    if(!output){
+        return strdup("Error internal.");
+    }
+
     dtw.remove_any("cache.txt");
     char *result_str = (char*)malloc(strlen(output) + 100);
     sprintf(result_str, "status: %d\nOutput:\n%s", result, output);
@@ -34,7 +38,7 @@ char *agent_execute_command(cJSON *args, void *pointer){
 }
 
 void configure_execute_command_callbacks(OpenAiInterface *openAi,const char *model){
-    OpenAiCallback *callback = new_OpenAiCallback(agent_execute_command, (void*)model, "execute_command", "execute a command", false);
+    OpenAiCallback *callback = new_OpenAiCallback(agent_execute_command, (void*)model, "execute_command", "execute a command", true);
     OpenAiInterface_add_parameters_in_callback(callback, "command", "Pass the command you want to execute.", "string", true);
     OpenAiInterface_add_callback_function_by_tools(openAi, callback);
 }
